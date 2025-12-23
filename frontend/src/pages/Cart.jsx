c8df1d2ca2267f8a721ee36b5a95ac9c635d90b8
saranya <saranyavanamutham@gmail.com>
2025-12-23 18:32:29 +0530

    Fix import case for CartTotal component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import Title from '../components/Title';
 import { assets } from '../assets/assets';
-import CartTotal from '../components/cartTotal';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
   const { products, cartItems, currency, updateQuantity, navigate } = useContext(ShopContext);
